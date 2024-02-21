14234591208c0430f8378620bff119e7bc7aa9f9
LuisRuiz09 <160760519+LuisRuiz09@users.noreply.github.com>
2024-02-21 11:12:42 -0500

    Add files via upload

---
+++ DataTypes/main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	float f1= 35e3;
+	double d1 = 12E4;
+	cout<<f1<<endl ;
+	cout<<d1;
+	return 0;
+}
